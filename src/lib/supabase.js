7bd816149f258fcc34c36e823bd890905ad8c537
Gerymvp <gretax25@gmail.com>
2026-05-05 19:24:54 -0500

    Agregar boton de carrito

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
@@ -5,3 +6,11 @@ export const supabase = createClient(
   import.meta.env.PUBLIC_SUPABASE_URL,
   import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 );
+=======
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+>>>>>>> 95d9f7d1aae0f6be883535e591ecb782c2e8cdcc
