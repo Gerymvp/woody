a3acbc6e74b8be627ba13ee26cc5ca58bd615715
elbandido16 <ecarvajal16@outlook.com>
2026-06-23 21:53:34 -0500

    registro y login

--- public/supabase.js
+++ public/supabase.js
@@ -1,6 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://zhxqrjtxcmubxquttbyx.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoeHFyanR4Y211YnhxdXR0Ynl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjU1MTEsImV4cCI6MjA5MzE0MTUxMX0.AfTnrBRYdbjT-TGdyoLYj6ZRlPkDS7uat69VmFQyHOs";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
