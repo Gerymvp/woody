fcbf2abf3b16c79276c3f2d48c7c913474984770
Gerymvp <gretax25@gmail.com>
2026-07-17 19:01:08 -0500

    vista a la bd en tiempo real

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'; // Puedes cambiarlo por @astrojs/netlify si usas Netlify
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Habilita el renderizado en el servidor en tiempo real
+  adapter: vercel(), // Indica el adaptador de la plataforma donde subes la web
   vite: {
     plugins: [tailwindcss()]
   }
